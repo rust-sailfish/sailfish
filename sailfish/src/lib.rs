4fef20fa86ba1496f047f11f6375b70d8b3ccc6a
Kogia-sima <orcinus4627@gmail.com>
2020-06-14 12:29:45 +0900

    Add crate-level documentation

--- sailfish/src/lib.rs
+++ sailfish/src/lib.rs
@@ -1,3 +1,36 @@
+//! Sailfish is a simple, small, and extremely fast template engine for Rust.
+//! Before reading this reference,
+//! I recommend reading [User guide](https://sailfish.netlify.app/en/).
+//!
+//! This crate contains utilities for rendering sailfish template.
+//! If you want to use sailfish templates, import `sailfish-macros` crate and use
+//! derive macro `#[derive(TemplateOnce)]` or `#[derive(Template)]`.
+//!
+//! In most cases you don't need to care about the `runtime` module in this crate, but
+//! if you want to render custom data inside templates, you must implement
+//! `runtime::Render` trait for that type.
+//!
+//! ```ignore
+//! #[macro_use]
+//! extern crate sailfish_macros;
+//!
+//! use sailfish::TemplateOnce;
+//!
+//! #[derive(TemplateOnce)]
+//! #[template(path = "hello.stpl")]
+//! struct HelloTemplate {
+//!     messages: Vec<String>
+//! }
+//!
+//! fn main() {
+//!     let ctx = HelloTemplate {
+//!         messages: vec!["foo".to_string(), "bar".to_string()]
+//!     };
+//!
+//!     println!("{}", ctx.render_once().unwrap());
+//! }
+//! ```
+
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/Kogia-sima/sailfish/master/resources/icon.png"
 )]
