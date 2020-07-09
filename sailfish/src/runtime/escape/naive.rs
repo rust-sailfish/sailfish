6c219551899e1d21a6c8d41703683954c9c18300
Kogia-sima <orcinus4627@gmail.com>
2020-07-10 07:38:06 +0900

    Fix memcpy_small implementation to emulate glibc implenetation exactly

--- sailfish/src/runtime/escape/naive.rs
+++ sailfish/src/runtime/escape/naive.rs
@@ -100,29 +100,30 @@ pub(super) unsafe fn escape_small(feed: &str, mut buf: *mut u8) -> usize {
 }
 
 /// memcpy implementation based on glibc (https://github.molgen.mpg.de/git-mirror/glibc/blob/master/sysdeps/x86_64/multiarch/memcpy-avx-unaligned.S)
-#[cfg_attr(feature = "perf-inline", inline)]
 #[allow(clippy::cast_ptr_alignment)]
 unsafe fn memcpy_small(src: *const u8, dst: *mut u8, len: usize) {
     debug_assert!(len <= 16);
-    if len >= 8 {
-        let tmp = ptr::read_unaligned(src as *const u64);
-        ptr::write_unaligned(dst as *mut u64, tmp);
+    let len_u8 = len as u8;
+
+    if len_u8 >= 8 {
         let offset = len - 8;
-        let tmp = ptr::read_unaligned(src.add(offset) as *const u64);
-        ptr::write_unaligned(dst.add(offset) as *mut u64, tmp);
-    } else if len >= 4 {
-        let tmp = ptr::read_unaligned(src as *const u32);
-        ptr::write_unaligned(dst as *mut u32, tmp);
+        let t2 = ptr::read_unaligned(src.add(offset) as *const u64);
+        let t1 = ptr::read_unaligned(src as *const u64);
+        ptr::write_unaligned(dst.add(offset) as *mut u64, t2);
+        ptr::write_unaligned(dst as *mut u64, t1);
+    } else if len_u8 >= 4 {
         let offset = len - 4;
-        let tmp = ptr::read_unaligned(src.add(offset) as *const u32);
-        ptr::write_unaligned(dst.add(offset) as *mut u32, tmp);
-    } else if len >= 2 {
-        let tmp = ptr::read_unaligned(src as *const u16);
-        ptr::write_unaligned(dst as *mut u16, tmp);
+        let t2 = ptr::read_unaligned(src.add(offset) as *const u32);
+        let t1 = ptr::read_unaligned(src as *const u32);
+        ptr::write_unaligned(dst.add(offset) as *mut u32, t2);
+        ptr::write_unaligned(dst as *mut u32, t1);
+    } else if len_u8 >= 2 {
         let offset = len - 2;
-        let tmp = ptr::read_unaligned(src.add(offset) as *const u16);
-        ptr::write_unaligned(dst.add(offset) as *mut u16, tmp);
-    } else if len >= 1 {
+        let t2 = ptr::read_unaligned(src.add(offset) as *const u16);
+        let t1 = ptr::read_unaligned(src as *const u16);
+        ptr::write_unaligned(dst.add(offset) as *mut u16, t2);
+        ptr::write_unaligned(dst as *mut u16, t1);
+    } else if len_u8 >= 1 {
         *dst = *src;
     }
 }
