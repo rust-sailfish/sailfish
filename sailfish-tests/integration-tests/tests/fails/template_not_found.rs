2cc9556dbe4324ac5d9e95395b31c43a1f94cafa
Kogia-sima <orcinus4627@gmail.com>
2021-02-03 15:58:51 +0900

    test: Improve `template_not_found` test to detect stable/nightly mismatch error
    cc #50

--- sailfish-tests/integration-tests/tests/fails/template_not_found.rs
+++ sailfish-tests/integration-tests/tests/fails/template_not_found.rs
@@ -2,11 +2,16 @@ use sailfish::TemplateOnce;
 use sailfish_macros::TemplateOnce;
 
 #[derive(TemplateOnce)]
+#[template(path = "empty.stpl")]
+struct ExistTemplate;
+
+#[derive(TemplateOnce)]
 #[template(path = "not_exist.stpl")]
 struct NotExistTemplate {
     var: usize
 }
 
 fn main() {
+    println!("{}", ExistTemplate.render_once().unwrap());
     println!("{}", NotExistTemplate { var: 1996 }.render_once().unwrap());
 }
